35eacde2b99144f9feeb647375da3e4c3401d23b
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-03-27 03:05:56 +0800

    Update main.py

--- Day 20 project/main.py
+++ Day 20 project/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,8 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -20,8 +23,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+        
+    if snake.head.distance(food) <= 15:
+        food.refresh()
     
 
 screen.exitonclick()
