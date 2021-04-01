12a9a4db361872a5536b12daac7c01cb6ec3100c
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-01 19:45:37 +0800

    Update snake.py

--- Day 20 project/snake.py
+++ Day 20 project/snake.py
@@ -12,15 +12,21 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-
+       
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color('white')
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+    
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+        
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -29,7 +35,6 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     
-
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
