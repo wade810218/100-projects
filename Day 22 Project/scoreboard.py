a9bf28dd2a63371f40c5329ffd8faa68551152d5
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-03 23:23:53 +0800

    Update scoreboard.py

--- Day 22 Project/scoreboard.py
+++ Day 22 Project/scoreboard.py
@@ -8,21 +8,17 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color('white')
         self.penup()
-        #self.goto(position)
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
         self.update_scoreboard()
-
 
     def update_scoreboard(self):
         self.clear()
         self.goto(-50 ,240)
         self.write(f'{self.l_score}',align=ALIGNMENT, font = FONT)
         self.goto(50 ,240)
-        self.write(f'{self.r_score}',align=ALIGNMENT, font = FONT)  
-        
-
+        self.write(f'{self.r_score}',align=ALIGNMENT, font = FONT)          
 
     def l_point(self):
         self.l_score += 1
