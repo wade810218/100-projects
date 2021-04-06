4084a44d5e0496bd76143120a7d3f5222aac88e4
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-06 19:37:13 +0800

    Update scoreboard.py

--- Day 23 Project/scoreboard.py
+++ Day 23 Project/scoreboard.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-270, 270)
+        self.level = 1
+        self.current_level()
+
+
+    def current_level(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def next_level(self):
+        self.level += 1
+        self.current_level()
