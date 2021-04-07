385fe320623a8ce0cececa45cd6ec21d35e2a882
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-07 19:56:59 +0800

    Update main.py

--- Day 23 Project/main.py
+++ Day 23 Project/main.py
@@ -5,11 +5,11 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=300, height=300)
 screen.tracer(0)
 
 player = Player()
-car_manager = CarManager()
+car_manager = []
 scoreboard = Scoreboard()
 
 screen.listen()
@@ -17,11 +17,22 @@ screen.onkey(player.crawl, "Up")
 
 
 game_is_on = True
+n=0
 while game_is_on:
     time.sleep(0.1)
+    n += 1
+    if n % 6:
+        
+        car_manager.append(CarManager())
     screen.update()
-    car_manager.drive()
+    car_manager[0].drive()
 
     if player.finish_check():
         scoreboard.next_level()
-        #car_manager.speedup()
+        car_manager.speedup()
+    
+    if player.distance(car_manager) < 20:
+        print('Gameover')
+        scoreboard.gameover()
+        game_is_on = False
+        
