bef303777aab47cf2aa8568527cd8f51ec9bcebe
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-07 19:57:15 +0800

    Update car_manager.py

--- Day 23 Project/car_manager.py
+++ Day 23 Project/car_manager.py
@@ -8,17 +8,25 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
+        self.cars = []
+
         self.shape('square')
+        self.shapesize(stretch_len =2, stretch_wid=1 )
         self.color(random.choice(COLORS))
         self.penup()
-        self.setheading(180)
-        self.goto(300, random.randrange(-300, 300))
+        self.setheading(180)        
+        self.goto(300, random.randrange(-250, 250))
+
         self.drive()
-    
+
+    def creat_car(self):
+        self.CarManager()
+
     def drive(self):
         self.forward(STARTING_MOVE_DISTANCE)
 
-    # def speedup(self):
-
-    #     return STARTING_MOVE_DISTANCE += MOVE_INCREMENT
+    def speedup(self):
+        global STARTING_MOVE_DISTANCE
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
+    #  return STARTING_MOVE_DISTANCE += MOVE_INCREMENT
     #it's global variable not suggest uset it in local score, try to use return method
