3af4e75ac6255f40c01739440e45594d33bf0604
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-13 18:11:34 +0800

    Update car_manager.py

--- Day 23 Project/car_manager.py
+++ Day 23 Project/car_manager.py
@@ -11,7 +11,7 @@ class CarManager(Turtle):
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
         self.hideturtle()
-
+        
     def create_car(self):
         random_chance = random.randint(1,6)
         if random_chance == 1:            
