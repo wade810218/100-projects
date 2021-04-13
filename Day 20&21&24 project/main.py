8d1a5e64a024814342b3dc4271c464c050b4a4d2
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-13 19:25:33 +0800

    Update and rename Day 20&21 project/main.py to Day 20&21&24 project/main.py

--- Day 20&21&24 project/main.py
+++ Day 20&21&24 project/main.py
@@ -24,26 +24,25 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.3)
     snake.move()
     
     #Detect collision with food.
     if snake.head.distance(food) <= 15:
         food.refresh()
         snake.extend()
-        scoreboard.score += 1
-        scoreboard.update_scoreboard()
+        scoreboard.increase_score()
     
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #Detec collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False 
-            scoreboard.game_over()
-            print('gameover')
+            scoreboard.reset()
+            snake.reset()
+            
 
 screen.exitonclick()
