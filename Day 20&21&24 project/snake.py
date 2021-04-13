e12af74018e3e5932b64dad590314e5fa1759466
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-04-13 19:28:29 +0800

    Update and rename Day 20&21 project/snake.py to Day 20&21&24 project/snake.py

--- Day 20&21&24 project/snake.py
+++ Day 20&21&24 project/snake.py
@@ -12,7 +12,7 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-       
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
@@ -26,7 +26,7 @@ class Snake:
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
-        
+
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -34,7 +34,13 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
-    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+        
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
