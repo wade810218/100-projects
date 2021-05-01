12b83a0681ae2a9c75921e9abca098d677f2c2d2
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-05-01 19:44:58 +0800

    Update scoreboard.py

--- Day 20&21&24 project/scoreboard.py
+++ Day 20&21&24 project/scoreboard.py
@@ -6,7 +6,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0    
-        self.high_score = 0
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.color('white')
@@ -24,10 +25,12 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt','w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
-        
-    # def game_over(self):
-    #     self.home()
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT )
+
+    #  def game_over(self):
+    #      self.home()
+    #      self.write("GAME OVER", align=ALIGNMENT, font=FONT )
 
