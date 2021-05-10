d63181e5644fbc4ad1f27992926a2e2507c7ade2
wade810218 <77575392+wade810218@users.noreply.github.com>
2021-05-11 01:43:10 +0800

    Update main.py

--- Day 25 Project/main.py
+++ Day 25 Project/main.py
@@ -10,36 +10,36 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-#3. Write correct guesses on to the map
+# Write correct guesses on to the map
 def write_state(name):
     x, y = data.loc[name]
     pen = turtle.Turtle()
     pen.hideturtle()
     pen.penup()  
     pen.goto(x, y)
-    pen.write(name)
-    
+    pen.write(name)    
 
-#5. Record the correct guesses in a list
+# Record the correct guesses in a list
 guessed_states = []
 
-#4. Use a loop to allow the user to keep guessing
+# Use a loop to allow the user to keep guessing
 while True:
-    #1. Convert the guess to Title classmethod
-    #6. Keep track of the score
+    # Convert the guess to Title classmethod
+    # Keep track of the score
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in data.index:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")        
+        break
 
-    #2. Check if the guess is among the 50 states
+    # Check if the guess is among the 50 states
     if answer_state in data.index:
         write_state(answer_state)
         guessed_states.append(answer_state)
-        
-
-print(turtle.position())
-turtle.goto(25,10)
-for name in data.index:
-    write_state(name)
-print(turtle.position())
 
 
